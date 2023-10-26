6e8516622706e32c5a7928591ac59b458025586d
Ekenechukwu Nwannunu <enwannun@pnw.edu>
2023-10-26 16:28:19 -0400

    Starting on Character Testing

--- src/test/java/com/levelup/forestsandmonsters/CharacterTest.java
+++ src/test/java/com/levelup/forestsandmonsters/CharacterTest.java
@@ -1,5 +1,11 @@
 package com.levelup.forestsandmonsters;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import com.levelup.forestsandmonsters.features.CreateCharacterSteps;
 
 public class CharacterTest {
+    GameController createCharacter = new GameController();
+    
     
 }
