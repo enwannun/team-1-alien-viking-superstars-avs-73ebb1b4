90676cd9583711efd05fc8380018a30f42bc4bab
Ekenechukwu Nwannunu <enwannun@pnw.edu>
2023-10-26 15:14:03 -0400

    Testing to fail to force moveCount to be the right number

--- src/test/java/com/levelup/forestsandmonsters/GameControllerTest.java
+++ src/test/java/com/levelup/forestsandmonsters/GameControllerTest.java
@@ -1,13 +1,24 @@
 package com.levelup.forestsandmonsters;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 
-public class GameControllerTest {
+import com.levelup.forestsandmonsters.GameController.GameStatus;
+
+import java.util.Scanner;
+
+public class GameControllerTest
+ {
     @Test
     public void initializationCreatesResults() {
         GameController testObj = new GameController();
         assertNotNull(testObj.status);
+        testObj.status.moveCount = -100;
+        assertEquals(0, testObj.status.moveCount); 
+        
     }
+    
+
 
 }
